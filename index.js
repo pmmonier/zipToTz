f6f9fc67e416ad4d27ac3a7ef619d5205ebe12d5
Pedro <pmmonier@buylottonow.con>
2020-07-30 18:40:10 -0400

    Adding service

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var tools_test_1 = require("./tools.test");
-Object.defineProperty(exports, "TestApi", { enumerable: true, get: function () { return tools_test_1.TestApi; } });
-Object.defineProperty(exports, "ITestApi", { enumerable: true, get: function () { return tools_test_1.ITestApi; } });
+var service_1 = require("./service");
+Object.defineProperty(exports, "ZipToTz", { enumerable: true, get: function () { return service_1.ZipToTz; } });
 //# sourceMappingURL=index.js.map
---
+++ service.js
@@ -0,0 +1,47 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ZipToTz = void 0;
+const fs = require('fs');
+const yaml = require('js-yaml');
+class ZipToTz {
+    short(zip) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.getTimeZone(zip, 'timezones_to_zipcodes');
+        });
+    }
+    full(zip) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.getTimeZone(zip, 'timezones_to_zipcodes(full name)');
+        });
+    }
+    getTimeZone(zip, fileName) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const value = zip.replace(/\s/g, '');
+            if (!Number.isInteger(Number(value)) || value.length != 5) {
+                console.log('Invalid format or zipCode length');
+            }
+            const filePath = process.cwd();
+            console.log(333, filePath);
+            let fileContents = yield fs.readFileSync(`${filePath}/.${fileName}.yml`, 'utf8');
+            let data = yaml.safeLoad(fileContents);
+            for (const [timezone] of Object.entries(data)) {
+                const found = data[timezone].find(element => element === value);
+                console.log(timezone);
+                if (found)
+                    return timezone;
+            }
+            return 'Not found';
+        });
+    }
+}
+exports.ZipToTz = ZipToTz;
+//# sourceMappingURL=service.js.map
