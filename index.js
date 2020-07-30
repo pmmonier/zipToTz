93da36c442060ac3585d11b87bc5dd38c8f0e3cf
Pedro <pmmonier@buylottonow.con>
2020-07-30 18:32:33 -0400

    Adding service.ts

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tools_test_1 = require("./tools.test");
+Object.defineProperty(exports, "TestApi", { enumerable: true, get: function () { return tools_test_1.TestApi; } });
+Object.defineProperty(exports, "ITestApi", { enumerable: true, get: function () { return tools_test_1.ITestApi; } });
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,1 @@
+export {ZipToTz} from './service';
