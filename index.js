4836f454a1eb5380d7faa13f8e9238389f05350f
Pedro <pmmonier@buylottonow.con>
2020-07-30 15:15:22 -0400

    Adding publish to command

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ZipToTz = void 0;
 const fs = require('fs');
 const yaml = require('js-yaml');
 class ZipToTz {
