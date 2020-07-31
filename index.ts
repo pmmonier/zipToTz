e0cb0e1bfef3843a63149c1575064fdbd2febcef
Pedro <pmmonier@buylottonow.con>
2020-07-31 10:27:41 -0400

    Removing promises

--- index.ts
+++ index.ts
@@ -2,24 +2,23 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 export class ZipToTz {
-    async short(zip: string): Promise<string> {
-        return await this.getTimeZone(zip, 'timezones_to_zipcodes');
+    short(zip: string) {
+        return this.getTimeZone(zip, 'timezones_to_zipcodes');
     }
 
-    async full(zip: string): Promise<string> {
-        return await this.getTimeZone(zip, 'timezones_to_zipcodes(full name)');
+    full(zip: string) {
+        return this.getTimeZone(zip, 'timezones_to_zipcodes(full name)');
     }
 
-    private async getTimeZone(zip: string, fileName: string): Promise<string> {
+    private getTimeZone(zip: string, fileName: string) {
         const value = zip.replace(/\s/g, '');
         if (!Number.isInteger(Number(value)) || value.length != 5) {
             console.log('Invalid format or zipCode length')
         }
         const filePath = process.cwd();
-        let fileContents = await fs.readFileSync(`${filePath}/node_modules/zip-to-timezone/src/.${fileName}.yml`, 'utf8');
-        let data = yaml.safeLoad(fileContents);
-        for (const [timezone] of Object.entries(data)) {
-            const found = data[timezone].find(element => element === value)
+        let zipList = yaml.safeLoad(fs.readFileSync(`${filePath}/node_modules/zip-to-timezone/src/.${fileName}.yml`, 'utf8'));
+        for (const [timezone] of Object.entries(zipList)) {
+            const found = zipList[timezone].find(element => element === value)
             if (found)
                 return timezone
         }
@@ -27,7 +26,9 @@ export class ZipToTz {
     }
 }
 
-
+const v = new ZipToTz()
+let g = v.full('99618');
+console.log(666, g)
 //Eastern Time  - America/New_York
 //Central Time  - America/Chicago
 // Mountain Time - America/Denver
